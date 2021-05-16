a8d2ddef036e69ec7138ab2ab0d9146c381aaf7e
Tiago Matos <picpay@PPM-SPO-14879.local>
2021-05-16 17:22:08 -0300

    first version pfa nginx as reverse proxy

---
+++ docker/pfa/node/index.js
@@ -0,0 +1,11 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) =>{
+    res.send("PFA Docker!")
+})
+
+app.listen(3000, () =>{
+    console.log("Server Rodando na porta 3000")
+})
